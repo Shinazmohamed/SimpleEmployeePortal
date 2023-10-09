50a20708815890cd353be23efa7204604705ce53
Shinazmohamed <115807856+Shinazmohamed@users.noreply.github.com>
2023-10-09 22:17:01 +0400

    Add project files.

---
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EmployeePortal.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
